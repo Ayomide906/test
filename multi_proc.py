88683e54090d787a2d554bfaaf197ac3524b64ba
Ayomide906 <akinsvibes17@gmail.com>
2025-07-05 10:21:29 +0100

    added hungry prompt

--- multi_proc.py
+++ multi_proc.py
@@ -1,16 +1,3 @@
-import time
-def time_it(func):
-    def wrapper(*args,**kwargs):
-        start=time.time()
-        result=func(*args,**kwargs)
-        end=time.time()
-        print(func.__name__ ," took ", (end-start)*1000, "mil sec" )
-        return result
-    return wrapper
-@time_it
-def ogba(alele="dont go"):
-    print(alele)
-
 import multiprocessing
 #create two processes , one to calc square and one to calc cube
 square_result=[]
@@ -24,17 +11,13 @@ def calc_square(numbers):
 def calc_cube(numbers):
     for n in numbers:
         print(f"cube of {n} is: " + str(n*n*n))
-def combined_target(numbers,func):
-    calc_cube(numbers)
-    time_it(func)
 if __name__=='__main__':
     arr=[2,3,8,9]
     p1=multiprocessing.Process(target=calc_square, args=(arr,))
-    p2=multiprocessing.Process(target=combined_target,args=(arr,))
+    p2=multiprocessing.Process(target=calc_cube,args=(arr,))
     p1.start()
     p2.start()
 
     p1.join()
     p2.join()
-    ogba()
     print("Done!")
